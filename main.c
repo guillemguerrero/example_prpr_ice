8ac4df609be016e6a335375a0a99397f82528d07
nuriaensenat <nuriaensenat@gmail.com>
2021-10-05 09:42:47 +0200

    Third commit

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 int main() {
 
 	printf("Menu\t 1. Create\t");	//First commit
-	printf("2.Update\n");		//Second commit
+	printf("2.Update\t");		//Second commit
+	printf("3. List\n");		//Third commit
 	
 	return 0;
 }
