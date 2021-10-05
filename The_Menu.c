91f7b8f24d5cfe71c359635888d7786d1e94d4fc
Laia Casas <laiacasas@MacBook-Pro-de-Laia.local>
2021-10-05 09:49:21 +0200

    First Commit

---
+++ The_Menu.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+int main(){
+
+	int option;
+
+	printf("1.Create|");
+	printf("2.Update|");
+	printf("3.List|");
+	printf("4.Delete");
+
+	scanf("%d", &option);
+
+	return 0;
+}
